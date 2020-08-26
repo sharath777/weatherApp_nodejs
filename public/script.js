2fd2f1ece4b4c4ff83e12f8e7df6d4cfcb8ff3da
sharath777 <vsharath2019@gmail.com>
2020-08-26 11:03:23 +0100

    Weather App
    Initial Commit

---
+++ public/script.js
@@ -0,0 +1,48 @@
+const searchElement = document.querySelector('[data-city-search]')
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed',() => {
+    const place = searchBox.getPlaces()[0]
+    if (place == null) return
+    const latitude = place.geometry.location.lat()
+    const longitude = place.geometry.location.lng()
+
+    fetch('/weather', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            latitude: latitude,
+            longitude: longitude   
+        })
+    }).then(res => res.json()).then(data => {
+        console.log("new data " +data)
+        setWeatherData(data, place.formatted_address)
+    })
+})
+const icon = new Skycons({ color: '#222' })
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const humidityElement = document.querySelector('[data-humidity]')
+const feelsLikeElement = document.querySelector('[data-feelsLike]')
+icon.set('icon', 'clear-day')
+icon.play()
+
+function setWeatherData(data, place) {
+    console.log("THIS IS A TEST")
+    // Round up the temp 
+   var celcius = Math.round(parseFloat(data.data.main.temp)-273.15);
+
+    locationElement.textContent = place
+    statusElement.textContent = data.weather.description
+    temperatureElement.textContent = celcius + '&deg;';
+
+    console.log("Temp in celcius " +celcius)
+    humidityElement.textContent = data.data.main.humidity
+    feelsLikeElement.textContent = data.main.feels_like
+    icon.set('icon', data.icon)
+    icon.play()
+
+}
